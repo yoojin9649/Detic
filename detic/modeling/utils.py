7e1eab2d8e9d44b87305bfc30e42ac0f4cf9b8fb
Yoojin Ha <59722489+yoojin9649@users.noreply.github.com>
2023-02-05 17:30:11 +0900

    Update utils.py

--- detic/modeling/utils.py
+++ detic/modeling/utils.py
@@ -6,7 +6,7 @@ from torch.nn import functional as F
 
 def load_class_freq(
     path='datasets/metadata/lvis_v1_train_cat_info.json', freq_weight=1.0):
-    cat_info = json.load(open(path, 'r'))
+    cat_info = json.load(open(os.path.join('Detic',path), 'r'))
     cat_info = torch.tensor(
         [c['image_count'] for c in sorted(cat_info, key=lambda x: x['id'])])
     freq_weight = cat_info.float() ** freq_weight
